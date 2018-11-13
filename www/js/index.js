80d655127eb51da3d7a4da6fd2455a254c7bd2b2
Mihai David <mihai.david@issco.ro>
2018-11-13 11:03:59 +0200

    Added demo

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,57 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
+    offset: 0,
+    albumsIds: [],
+
     initialize: function() {
+        var btnEl = document.getElementById('loadMoreBtn');
+        btnEl.onclick = function() {
+            app.onLoadMoreClick();
+        };
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        var parentElement = document.getElementById('albums');
+        Photos.collections({"collectionMode": "ALBUMS"},
+            function(albums) {
+                for (var i=0; i<albums.length; i++) {
+                    app.albumsIds.push(albums[i].id);
+                    var para = document.createElement("P");
+                    var t = document.createTextNode(albums[i].name);
+                    para.appendChild(t);
+                    parentElement.appendChild(para);
+                }
+                app.addPhotos();
+            },
+            function(error) {
+                console.error("Error: " + error);
+            });
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    onLoadMoreClick() {
+        app.offset += 10;
+        app.addPhotos();
+    },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    addPhotos() {
+        var parentElement = document.getElementById('photos');
+        Photos.photos(app.albumsIds,
+            {"offset": app.offset, "limit": 10},
+            function(photos) {
+                Photos.cancel();
+                for (var i=0; i<photos.length; i++) {
+                    Photos.thumbnail(photos[i].id,
+                        {"asDataUrl": true, "dimension": 100, "quality": 70},
+                        function(data) {
+                            var img = document.createElement("img");
+                            img.src = data;
+                            parentElement.appendChild(img);
+                        },
+                        function(error) {
+                            console.error("Error: " + error);
+                        });
+                }
+        }, console.error);
     }
 };
 
